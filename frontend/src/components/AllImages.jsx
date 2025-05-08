5302bd1582e0e4decd9f0a14ba72435bb543b60b
Shiva Verma <shivaverma30july@gmail.com>
2025-05-08 14:25:55 +0530

    ui home changed

--- frontend/src/components/AllImages.jsx
+++ frontend/src/components/AllImages.jsx
@@ -9,7 +9,7 @@ const AllImages = () => {
     useEffect(() => {
         fetch("https://conference-website-dnqi.onrender.com/photogallery/all")
             .then(response => response.json())
-            .then(data => setImages(data.slice(0, 6))) // Show only 6 images
+            .then(data => setImages(data)) // Show only 6 images
             .catch(error => console.error("Error fetching images:", error));
     }, []);
 

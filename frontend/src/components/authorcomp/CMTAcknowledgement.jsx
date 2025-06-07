f36a8eb6f6e0c8c53a310131194a66fce28b3aba
Suryansh Verma <suryanshv.ug23.ee@nitp.ac.in>
2025-06-07 14:30:46 +0530

    cmt acknowledgement txt to italic

--- frontend/src/components/authorcomp/CMTAcknowledgement.jsx
+++ frontend/src/components/authorcomp/CMTAcknowledgement.jsx
@@ -4,7 +4,7 @@ const CMTAcknowledgement = () => {
   return (
     <div className='mt-20'>
         <h3 className="sm:text-3xl lg:text-4xl text-2xl font-semibold text-gray-800 mt-6 underline text-center p-5">CMT Acknowledgement</h3> {/* No underline here */}
-        <p className='list-disc lg:px-20 sm:px-10 px-6 text-gray-700 space-y-2 text-xl'>The <span className='text-blue-600'>Microsoft CMT service</span> was used for the managing the peer-reviewing process for this conference. This service was provided for free by Microsoft and they bore all expenses, including costs for Azure cloud services as well as for software development and support.</p>
+        <p className='list-disc lg:px-20 sm:px-10 px-6 text-gray-700 space-y-2 text-xl italic'>"The Microsoft CMT service was used for managing the peer-reviewing process for this conference. This service was provided for free by Microsoft and they bore all expenses, including costs for Azure cloud services as well as for software development and support."</p>
     </div>
   )
 }

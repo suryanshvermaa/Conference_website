d419bb64f82cb1d6f553ef7d2ce449f33549bc6c
Prabhat <87269080+Prabhatsingh001@users.noreply.github.com>
2026-04-15 20:29:19 +0530

    fix: set trust proxy for handling forwarded requests

--- backend/src/index.js
+++ backend/src/index.js
@@ -7,6 +7,8 @@ const clc=require("cli-color")
 const router = require("./routes")
 dotenv.config();
 
+app.set("trust proxy", true);
+
 app.use(cors({origin:["https://icnari26.nasl.in","https://conference-website-three.vercel.app","http://localhost:5173"]}))
 app.use(express.json())
 app.use("/",router)

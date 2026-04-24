56e7fce52223c5e801c78be4a58130ba9a7ae928
Shivang Jaiswal <jaiswalshivang01@gmail.com>
2026-04-24 21:43:05 +0530

    Added client side verification

--- backend/src/routes/contactroute.js
+++ backend/src/routes/contactroute.js
@@ -7,10 +7,10 @@ const { body } = require('express-validator');
 
 contactRouter
 .post("/",middlewares.contactLimiter, [
-    body('name').notEmpty().withMessage('Name is required'),
+    body('name').isLength({ min: 2 }).withMessage('Name must be at least 2 characters'),
     body('email').isEmail().withMessage('Please provide a valid email'),
-    body('subject').notEmpty().withMessage('Subject is required'),
-    body('message').notEmpty().withMessage('Message is required'),
+    body('subject').isLength({ min: 5 }).withMessage('Subject must be at least 5 characters'),
+    body('message').isLength({ min: 20 }).withMessage('Message must be at least 20 characters'),
     body('phone').optional().isMobilePhone().withMessage('Please provide a valid phone number')
 ],createContact)
 .get('/',auth,getContact)
--- backend/src/routes/organisingcommitteeRoutes.js
+++ backend/src/routes/organisingcommitteeRoutes.js
@@ -1,10 +1,17 @@
 const {Router} = require('express');
 const { createMember,getAllMembers,deleteMember, updateMember, getMember, setPriority }=require("../controllers/organisingmembercontroller");
 const auth = require('../middlewares/middleware');
+const { body } = require('express-validator');
 const organisingCommitteeRouter=Router();
 
 organisingCommitteeRouter
-.post('/createMember',auth,createMember)
+.post('/createMember',auth, [
+  body('name').isLength({ min: 2 }).withMessage('Name must be at least 2 characters'),
+  body('specialization').isLength({ min: 3 }).withMessage('Specialization must be at least 3 characters'),
+  body('college').isLength({ min: 2 }).withMessage('College must be at least 2 characters'),
+  body('description').isLength({ min: 10 }).withMessage('Description must be at least 10 characters'),
+  body('committee').notEmpty().withMessage('Committee is required'),
+],createMember)
 .get('/getAllMembers',getAllMembers)
 .delete('/deleteMember/:id',auth,deleteMember)
 .put('/updateMember/:id',auth,updateMember)
